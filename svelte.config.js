8824dd3970e06c73b5db70e9771164a1485efb1b
Tran Sang <me@transa.ng>
2023-06-09 02:11:11 +0900

    firebase

--- svelte.config.js
+++ svelte.config.js
@@ -2,24 +2,9 @@ import adapter from '@sveltejs/adapter-static'
 import {vitePreprocess} from '@sveltejs/kit/vite'
 
 /** @type {import('@sveltejs/kit').Config} */
-const config = {
-	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
-	// for more information about preprocessors
+export default {
 	preprocess: vitePreprocess(),
-
 	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter({
-			trailingSlash: 'always',
-			pages: 'build',
-			assets: 'build',
-			fallback: undefined,
-			precompress: false,
-			strict: true
-		})
+		adapter: adapter()
 	}
 }
-
-export default config
